30b4f5cd649e8851184b495eadac455d88a5cfb1
unknown <judah.tanninen@rotschyinc.com>
2020-11-13 15:43:55 -0800

    l

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://www.rotschyinc.com/x/cdn/?https://storage.googleapis.com/wzukusers/user-28233985/images/594ae164804b5eknjEXB/Picture5_d200.png">
+    <img alt="Rotschy inc Logo" src="https://www.rotschyinc.com/x/cdn/?https://storage.googleapis.com/wzukusers/user-28233985/images/594ae164804b5eknjEXB/Picture5_d200.png">
     <HelloWorld msg="Rotschy inc Employee Access"/>
+    <div>
+    </div>
   </div>
 </template>
 
